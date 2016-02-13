305ee57704bd49ecaffecae3c0a4dcdd9e1764f6
Drew Smith <drunet@gmail.com>
2016-02-12 19:43:00 -0500

    Update notifyTemp.py

--- notifyTemp.py
+++ notifyTemp.py
@@ -4,27 +4,22 @@ import os
 import time
 import datetime
 import glob
-import MySQLdb
 from time import strftime
  
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
-temp_sensor = '/sys/bus/w1/devices/28-00000622fd44/w1_slave'
- 
-# Variables for MySQL
-db = MySQLdb.connect(host="localhost", user="root",passwd="password", db="temp_database")
-cur = db.cursor()
+temp_sensor = '/sys/bus/w1/devices/28-0215658c80ff/w1_slave'
  
 def tempRead():
-    t = open(temp_sensor, 'r')
-    lines = t.readlines()
-    t.close()
+        t = open(temp_sensor, 'r')
+        lines = t.readlines()
+        t.close()
  
-    temp_output = lines[1].find('t=')
-    if temp_output != -1:
-        temp_string = lines[1].strip()[temp_output+2:]
-        temp_c = (float(temp_string)/1000.0*9/5+32)
-    return round(temp_c,1)
+        temp_output = lines[1].find('t=')
+        if temp_output != -1:
+                temp_string = lines[1].strip()[temp_output+2:]
+                temp_c = (float(temp_string)/1000.0*9/5+32)
+        return round(temp_c,1)
 
 now = datetime.datetime.now().strftime('%H:%M')
 
